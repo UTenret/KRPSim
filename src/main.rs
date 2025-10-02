cb2ddad3a6bf549f6f8c7802dc0bdbb181822570
Ulysse TENRET <ulysse.tenret@gmail.com>
2025-10-02 16:12:57 +0200

    how to read a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
+const CONFIG_DIR_PATH: &str = "config_files";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    let contents = fs::read_to_string(file_path);
+
+    println!("File contents : {}", contents.unwrap());
 }
